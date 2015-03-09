6ab9185e3817d508b9355d20503539adb9fa468b
dimon <its2exp@gmail.com>
2015-03-09 17:01:07 +0200

    Refactor.

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -5,12 +5,7 @@ class GuildsController < ApplicationController
 
   def show
     @guild = Guild.find(params[:id])
-    #@roster = @guild.epgps.where(recorded_at: 1420825500)
-    if params[:recorded_at]
-      @roster = @guild.epgps.where(recorded_at: params[:recorded_at])
-    else 
-      @roster = @guild.epgps.where(recorded_at: @guild.list_timestamps.max)
-    end
+    @roster = @guild.roster_on_timestamp( params[:recorded_at])
   end
 
   private
--- app/models/guild.rb
+++ app/models/guild.rb
@@ -5,4 +5,13 @@ class Guild < ActiveRecord::Base
   def list_timestamps
     self.epgps.select(:recorded_at).map(&:recorded_at).uniq
   end
+
+  def roster_on_timestamp(recorded_at)
+    if recorded_at.present?
+      self.epgps.where(recorded_at: recorded_at)
+    else
+      self.epgps.where(recorded_at: self.list_timestamps.max )
+    end
+  end
+
 end
