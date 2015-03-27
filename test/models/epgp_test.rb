4e780d72652ec6675b354a39934cf284248e8be5
dimon <its2exp@gmail.com>
2015-03-27 13:01:18 +0200

    guild tests

--- test/models/epgp_test.rb
+++ test/models/epgp_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class EpgpTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @one = epgps(:one)
+  end
+
+  test "loot_priority should return string" do
+    assert @one.loot_priority.is_a? String
+  end
+
+  test "loot_priority output format must be .3f" do
+    assert_equal @one.loot_priority, "2.500"
+  end
 end
