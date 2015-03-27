de8da2064d890fdee4c504875fa30b54ba84ff8c
dimon <its2exp@gmail.com>
2015-03-27 16:32:22 +0200

    more tests

--- test/models/epgp_parser_test.rb
+++ test/models/epgp_parser_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class EpgpParserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup 
+   @uploader = EpgpParser.new
+   @uploader.source = '{"guild":"Aware","realm":"Defias Brotherhood","region":"eu","roster":[["Abalith",10116,1736],["Anydia",17571,2891],["Ascl\u00e9pius",9558,407],["Browndiamond",228,1],["Byulshin",7938,1082],["Cacha\u00e7a",884,1]],"timestamp":1424804640}'
+ end
+
+ test "json_format should return true value if given data is valid" do
+   assert @uploader.json_format
+ end
+
+ test "json_format should return error if given data is invalid" do
+   @uploader.source = "test"
+   @uploader.json_format
+   assert @uploader.errors.any?
+ end
+
+ test "json_format should not add error if given data is valid" do
+   @uploader.json_format
+   assert !@uploader.errors.any?
+ end
 end
