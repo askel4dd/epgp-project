23cd07d8dcecfa02ef5e6c9230a9f68e57a47163
dimon <its2exp@gmail.com>
2015-03-27 18:46:55 +0200

    epgp_parser model tests

--- test/models/epgp_parser_test.rb
+++ test/models/epgp_parser_test.rb
@@ -20,4 +20,26 @@ class EpgpParserTest < ActiveSupport::TestCase
    @uploader.json_format
    assert !@uploader.errors.any?
  end
+
+ test "guild_owner should return true if guild in source data does not exist in database" do
+   @uploader.source = '{"guild":"Fargo","region":"eu","realm":"Warsong"}'
+   assert @uploader.guild_owner(users(:one))
+ end
+
+ test "guild_owner should return true if current_user and guild owner user equal" do
+   @uploader.source = '{"guild":"Mesmerize","region":"EU","realm":"Shadowmoon"}'
+   assert @uploader.guild_owner(users(:one))
+ end
+
+ test "guild_owner should return false if current_user does not equal guild owner user" do
+   @uploader.source = '{"guild":"Mesmerize","region":"EU","realm":"Shadowmoon"}'
+   assert !@uploader.guild_owner(users(:two))
+ end
+
+ test "guild should be saved for current_user" do
+   @uploader.json_format
+   @uploader.save(users(:one))
+   guild = Guild.find_by(name: "Aware", region: "eu", realm: "Defias Brotherhood")
+   assert_equal guild.user, users(:one)
+ end
 end
